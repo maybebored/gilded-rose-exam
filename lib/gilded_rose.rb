f8fb1760dd5e743b1c0a5003a543e72fbdc30a3b
Mayuran S <mayuselvaraja@gmail.com>
2019-12-16 23:47:38 +0800

    Add special methods to update different categories of items

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -17,4 +17,56 @@ class GildedRose
       end
     end
   end
+
+  def change_quality(current_quality, value, is_increase)
+    if is_increase
+      if current_quality+value < GildedRoseConstants::MAX_QUALITY
+        return current_quality+value
+      end
+    else
+      if current_quality-value > GildedRoseConstants::MIN_QUALITY
+        return current_quality-value
+      end
+    end
+    return current_quality
+  end
+
+  def update_aged_brie(item)
+    item.quality = change_quality(item.quality, 1, true)
+    item.sell_in = item.sell_in - 1
+
+    return item
+  end
+
+  def update_backstage_passes(item)
+    item.quality = change_quality(item.quality, 1, true)
+    if item.sell_in <= 10
+      item.quality = change_quality(item.quality, 1, true)
+    end
+    if item.sell_in <= 5
+      item.quality = change_quality(item.quality, 1, true)
+    end
+    if item.sell_in <= GildedRoseConstants::MIN_SELL_IN
+      item.quality = 0
+    end
+    return item
+  end
+
+  def update_conjured(item)
+    item.quality = change_quality(item.quality, 2, false)
+    item.sell_in = item.sell_in - 1
+
+    return item
+  end
+
+  def update_normal(item)
+    if item.sell_in <= GildedRoseConstants::MIN_SELL_IN
+      item.quality = change_quality(item.quality, 2, false)
+    else
+      item.quality = change_quality(item.quality, 1, false)
+    end
+
+    item.sell_in = item.sell_in - 1
+    return item
+  end
 end
