fc08e6b30204f0796060e4fe51981f716015163c
Mayuran S <mayuselvaraja@gmail.com>
2019-12-18 19:21:35 +0800

    Add item category names to constants file

--- lib/gilded_rose_constants.rb
+++ lib/gilded_rose_constants.rb
@@ -3,4 +3,9 @@ module GildedRoseConstants
   MIN_QUALITY=0
   MAX_QUALITY=50
   SULFURAS_QUALITY=80
+
+  ITEM_AGED_BRIE='Aged Brie'
+  ITEM_SULFURAS='Sulfuras, Hand of Ragnaros'
+  ITEM_BACKSTAGE_PASSES='Backstage passes to a TAFKAL80ETC concert'
+  ITEM_CONJURED='Conjured'
 end
